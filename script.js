ad86f3404b4fb6fd1f90bc589d2e7ac17782224a
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-04-09 18:56:44 +0200

    feat: update innerText of category-name class

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const mainSection = document.getElementById('main-section');
 const formSection = document.getElementById('form-section');
+const bookmarkListSection = document.getElementById('bookmark-list-section');
 // const categoryDropdown = document.getElementById('category-dropdown');
 const addBookmarkButton = document.getElementById('add-bookmark-button');
+const addBookmarkButtonForm = document.getElementById('add-bookmark-button-form');
+const closeFormButton = document.getElementById('close-form-button');
+const viewCategoryButton = document.getElementById('view-category-button');
+const nameInput = document.getElementById('name');
+const url = document.getElementById('url');
+const categoryList = document.getElementById('category-list');
+
 const categoryNames = document.getElementsByClassName('category-name');
+
+const category = document.querySelector('option:checked')
 
 const getBookmarks = () => {
     try {
@@ -25,7 +35,27 @@ const showSection = section => {
 const displayOrCloseForm = () => showSection(mainSection.classList.contains('hidden') ? mainSection : formSection);
 
 addBookmarkButton.addEventListener('click', (e) => {
-    const selectedCategory = document.querySelector('option')
-    // debugger;
-    categoryNames.forEach(el => el.innerText = ``);
+    Array.from(categoryNames).forEach(el => el.innerText = category.value);
+    displayOrCloseForm();
 })
+
+closeFormButton.addEventListener('click', displayOrCloseForm);
+
+addBookmarkButtonForm.addEventListener('click', (e) => {
+    const bookmarks = getBookmarks();
+    bookmarks.push({ name: nameInput.value, category: category.value, url: url.value });
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    nameInput.value = '';
+    url.value = '';
+    displayOrCloseForm();
+});
+
+const displayOrHideCategory = () => showSection(mainSection.classList.contains('hidden') ? mainSection : bookmarkListSection);
+
+viewCategoryButton.addEventListener('click', (e) => {
+
+    categoryList.innerHTML = '<p>No Bookmarks Found</p>';
+})
+
+
+// debugger;
