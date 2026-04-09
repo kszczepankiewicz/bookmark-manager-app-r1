366759cce9a88449254b659ace39ce23bc4f1a42
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-04-09 18:01:12 +0200

    feat: add toggling hidden class on main and form section

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-
+const mainSection = document.getElementById('main-section');
+const formSection = document.getElementById('form-section');
+// const categoryDropdown = document.getElementById('category-dropdown');
+const addBookmarkButton = document.getElementById('add-bookmark-button');
+const categoryNames = document.getElementsByClassName('category-name');
 
 const getBookmarks = () => {
     try {
         let bookmarks = localStorage.getItem('bookmarks');
         if (!bookmarks) return [];
-        // debugger;
         bookmarks = JSON.parse(bookmarks);
         if (!Array.isArray(bookmarks)) return [];
         if (!bookmarks.every(b => ['name', 'category', 'url'].every(p => p in b))) return [];
@@ -15,7 +18,14 @@ const getBookmarks = () => {
 }
 
 const showSection = section => {
-
+    [mainSection, formSection].forEach(s => s.classList.add('hidden'));
+    section.classList.remove('hidden');
 }
 
-const displayOrCloseForm = () => { }
+const displayOrCloseForm = () => showSection(mainSection.classList.contains('hidden') ? mainSection : formSection);
+
+addBookmarkButton.addEventListener('click', (e) => {
+    const selectedCategory = document.querySelector('option')
+    // debugger;
+    categoryNames.forEach(el => el.innerText = ``);
+})
