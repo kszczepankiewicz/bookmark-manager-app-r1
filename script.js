ee56c581f0809684524f3c3c1912462f5a22ab2f
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-04-10 15:03:47 +0200

    feat: add closing bookmark list

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const formSection = document.getElementById('form-section');
 const bookmarkListSection = document.getElementById('bookmark-list-section');
 // const categoryDropdown = document.getElementById('category-dropdown');
 const addBookmarkButton = document.getElementById('add-bookmark-button');
+const deleteBookmarkButton = document.getElementById('delete-bookmark-button');
 const addBookmarkButtonForm = document.getElementById('add-bookmark-button-form');
 const closeFormButton = document.getElementById('close-form-button');
+const closeListButton = document.getElementById('close-list-button');
 const viewCategoryButton = document.getElementById('view-category-button');
 const nameInput = document.getElementById('name');
 const url = document.getElementById('url');
@@ -12,7 +14,6 @@ const categoryList = document.getElementById('category-list');
 
 const categoryNames = document.getElementsByClassName('category-name');
 
-const category = document.querySelector('option:checked')
 
 const getBookmarks = () => {
     try {
@@ -28,13 +29,16 @@ const getBookmarks = () => {
 }
 
 const showSection = section => {
-    [mainSection, formSection].forEach(s => s.classList.add('hidden'));
+    [mainSection, formSection, bookmarkListSection].forEach(s => s.classList.add('hidden'));
     section.classList.remove('hidden');
 }
 
 const displayOrCloseForm = () => showSection(mainSection.classList.contains('hidden') ? mainSection : formSection);
 
+let category;
+
 addBookmarkButton.addEventListener('click', (e) => {
+    category = document.querySelector('option:checked');
     Array.from(categoryNames).forEach(el => el.innerText = category.value);
     displayOrCloseForm();
 })
@@ -52,10 +56,37 @@ addBookmarkButtonForm.addEventListener('click', (e) => {
 
 const displayOrHideCategory = () => showSection(mainSection.classList.contains('hidden') ? mainSection : bookmarkListSection);
 
-viewCategoryButton.addEventListener('click', (e) => {
+const displayBookmarks = (bookmarks) => bookmarks.map(b => `<input type='radio' name='${b.category}' id='${b.name}' value='${b.name}'><label for='${b.name}'><a href='${b.url}'>${b.name}</a></label>`).join('\n')
 
-    categoryList.innerHTML = '<p>No Bookmarks Found</p>';
+viewCategoryButton.addEventListener('click', (e) => {
+    showSection(bookmarkListSection);
+    category = document.querySelector('option:checked');
+    const bookmarks = getBookmarks().filter(b => b.category === category.value);
+    if (!bookmarks.length) {
+        categoryList.innerHTML = '<p>No Bookmarks Found</p>';
+        return
+    }
+    categoryList.innerHTML = displayBookmarks(bookmarks);
 })
 
+closeListButton.addEventListener('click', (e) => showSection(mainSection));
 
-// debugger;
+deleteBookmarkButton.addEventListener('click', (e) => {
+    const bookmarkToDelete = document.querySelector('input[type="radio"]:checked');
+
+    if (!bookmarkToDelete) {
+        alert('Bookmark not selected')
+        return;
+    }
+
+    const bookmarks = getBookmarks();
+    const index = bookmarks.findIndex(b => b.name === bookmarkToDelete.value && b.category === category)
+    if (index === -1) {
+        alert('Bookmark not found');
+        return;
+    }
+    bookmarks.splice(index, 1)
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    categoryList.innerHTML = displayBookmarks(bookmarks.filter(b => b.category === category.value)) || '<p>No Bookmarks Found</p>';
+
+})
